e43298aca17a2194aaf39963c903f8baf82ff8de
bax1234567890 <bota.ion.oleg@gmail.com>
2024-02-23 00:39:04 -0800

    Added couple of example for syntax.py

--- lessons_python/syntax.py
+++ lessons_python/syntax.py
@@ -1,2 +1,26 @@
 
 print('Hello there!\nHow are you?\nI\'m fine' )
+
+# ROW String
+print(r'Hello is Carol\'s cat?')
+
+# Multiple Strings with Triple Quotes
+print("""Hello, How are you?
+If good just answer to me.
+If not just be mute""")
+
+spam = """Hello, How are you?
+If good just answer to me.
+If not just be mute"""
+
+print(len(spam))
+
+
+# Similarites between list and string
+print(spam[0])
+print(spam[1:5])
+print(spam[-1])
+print('Hello' in spam)
+print('x' in spam)
+print('HELLO' in spam)
+
